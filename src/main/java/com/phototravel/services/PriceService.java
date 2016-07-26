5411a1ad6b99bafd04945f780ba0631221c24355
Olga_Govor <Olga_Govor@epam.com>
2016-07-26 14:00:15 +0200

    add price service

--- src/main/java/com/phototravel/services/PriceService.java
+++ src/main/java/com/phototravel/services/PriceService.java
@@ -1,7 +1,24 @@
 package com.phototravel.services;
 
+import com.phototravel.entity.Price;
+import com.phototravel.repository.PriceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.sql.Time;
+
 /**
  * Created by Olga_Govor on 7/26/2016.
  */
+@Service
 public class PriceService {
+
+    @Autowired
+    PriceRepository priceRepository;
+
+    public void createPrice(Integer routeId, Date departureDate, Time departureTime, double priceNum, Date lastUpdate) {
+        Price price = new Price(routeId, departureDate, departureTime, priceNum,lastUpdate);
+        priceRepository.save(price);
+    }
 }
