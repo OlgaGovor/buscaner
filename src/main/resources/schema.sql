3ef123930e3be4e0f1bfebe03c1ffe4619cc9637
PBezdienezhnykh <PBezdienezhnykh@luxoft.com>
2016-07-27 13:59:51 +0200

    added constraint UNIQUE to country_name

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,13 +2,15 @@ CREATE TABLE IF NOT EXISTS company (
   company_id   INT          NOT NULL AUTO_INCREMENT,
   company_name VARCHAR(100) NOT NULL,
   company_url  VARCHAR(200),
-  PRIMARY KEY (company_id)
+  PRIMARY KEY (company_id),
+  CONSTRAINT uc_company_name UNIQUE (company_name)
 );
 
 CREATE TABLE IF NOT EXISTS country (
   country_id   INT          NOT NULL AUTO_INCREMENT,
   country_name VARCHAR(100) NOT NULL,
-  PRIMARY KEY (country_id)
+  PRIMARY KEY (country_id),
+  CONSTRAINT uc_country_name UNIQUE (country_name)
 );
 
 CREATE TABLE IF NOT EXISTS city (
@@ -16,7 +18,8 @@ CREATE TABLE IF NOT EXISTS city (
   city_name  VARCHAR(100) NOT NULL,
   country_id INT          NOT NULL,
   PRIMARY KEY (city_id),
-  FOREIGN KEY (country_id) REFERENCES country (country_id)
+  FOREIGN KEY (country_id) REFERENCES country (country_id),
+  CONSTRAINT uc_city_name UNIQUE (city_name)
 );
 
 
@@ -35,11 +38,15 @@ CREATE TABLE IF NOT EXISTS route (
   route_id            INT NOT NULL AUTO_INCREMENT,
   from_destination_id INT NOT NULL,
   to_destination_id   INT NOT NULL,
+  from_city_id INT NOT NULL,
+  to_city_id   INT NOT NULL,
   company_id          INT NOT NULL,
   is_active           BOOLEAN,
   PRIMARY KEY (route_id),
   FOREIGN KEY (from_destination_id) REFERENCES destination (destination_id),
   FOREIGN KEY (to_destination_id) REFERENCES destination (destination_id),
+  FOREIGN KEY (from_city_id) REFERENCES city (city_id),
+  FOREIGN KEY (to_city_id) REFERENCES city (city_id),
   FOREIGN KEY (company_id) REFERENCES company (company_id)
 );
 
