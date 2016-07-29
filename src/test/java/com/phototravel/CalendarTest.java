5007f79a8c1afb8ac82f4c267717055050fbb93f
PabloMoneylesh <carmbu77>
2016-07-29 20:56:10 +0200

    calendar test

--- src/test/java/com/phototravel/CalendarTest.java
+++ src/test/java/com/phototravel/CalendarTest.java
@@ -6,6 +6,7 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by PBezdienezhnykh on 029 29.7.2016.
@@ -13,22 +14,37 @@ import java.util.Calendar;
 public class CalendarTest {
     @Test
     public void test() throws ParseException {
-        Calendar cal = Calendar.getInstance();
-        cal.setFirstDayOfWeek(Calendar.MONDAY);
+        String dateStartStr = "21-07-2016";
+        String dateEndStr = "15-09-2016";
+
         DateFormat df = new SimpleDateFormat("dd-MM-yyyy");
 
-        String dateStart = "28-07-2016";
-        String dateEnd = "15-09-2016";
-
-        cal.setTime(df.parse(dateStart));
-        cal.set(Calendar.DAY_OF_WEEK, cal.getFirstDayOfWeek());
-
+        Calendar startDate = Calendar.getInstance();
+        Calendar endDate = Calendar.getInstance();
+        startDate.setFirstDayOfWeek(Calendar.MONDAY);
+        endDate.setFirstDayOfWeek(Calendar.MONDAY);
 
 
-        System.out.println(df.format(cal.getTime()));
+        startDate.setTime(df.parse(dateStartStr));
+        startDate.set(Calendar.DAY_OF_WEEK, startDate.getFirstDayOfWeek());
+
+        endDate.setTime(df.parse(dateEndStr));
+        endDate.set(Calendar.DAY_OF_WEEK, startDate.getFirstDayOfWeek());
+        endDate.add(Calendar.DATE, 6);
 
 
-        System.out.println(df.format(cal.getTime()));
+
+
+
+
+        while(startDate.before(endDate)){
+            System.out.println(df.format(startDate.getTime()));
+            startDate.add(Calendar.DATE, 1);
+        }
+        System.out.println(df.format(endDate.getTime()));
+
+
+
 
     }
 }
