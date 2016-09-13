1b7812add64b5cfd89e29f6dbe679c62b16f36a4
PBezdienezhnykh <PBezdienezhnykh@luxoft.com>
2016-09-13 17:16:35 +0200

    added multi language support

--- src/main/java/com/phototravel/BuscanerApplication.java
+++ src/main/java/com/phototravel/BuscanerApplication.java
@@ -1,16 +1,74 @@
 package com.phototravel;
 
+import com.phototravel.services.Scrapper;
+import com.phototravel.services.impl.LuxexpressFetcher;
+import com.phototravel.services.impl.PolskiBusFetcher;
+import com.phototravel.services.impl.ScrapperImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
+import java.util.Locale;
+
+@Configuration
 @SpringBootApplication
 @EnableCaching
 @EnableScheduling
-public class BuscanerApplication {
+
+public class BuscanerApplication extends WebMvcConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BuscanerApplication.class, args);
 	}
+
+	@Bean
+	public Scrapper scrapper() {
+		Scrapper scrapper = new ScrapperImpl();
+		scrapper.register(PolskiBusFetcher.COMPANY_ID, new PolskiBusFetcher());
+		scrapper.register(LuxexpressFetcher.COMPANY_ID, new LuxexpressFetcher());
+		//register all fetchers
+		return scrapper;
+	}
+
+
+	@Bean
+	public MessageSource messageSource() {
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setBasename("classpath:i18n/messages/messages");
+		messageSource.setFallbackToSystemLocale(false);
+		messageSource.setDefaultEncoding("UTF-8");
+		return messageSource;
+	}
+
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver slr = new SessionLocaleResolver();
+		slr.setDefaultLocale(Locale.ENGLISH);
+		return slr;
+	}
+
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+		LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+		lci.setParamName("lang");
+		return lci;
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(localeChangeInterceptor());
+	}
+
+
 }
