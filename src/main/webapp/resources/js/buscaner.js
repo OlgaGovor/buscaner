956bb0a0ccecea8ee094c33909b6a79668ef26e9
PBezdienezhnykh <PBezdienezhnykh@luxoft.com>
2016-10-18 16:46:49 +0200

    main page structure and style improved

--- src/main/webapp/resources/js/buscaner.js
+++ src/main/webapp/resources/js/buscaner.js
@@ -1,20 +1,24 @@
-function initForm(){
+function initForm() {
     setupEnv();
     initDatePickers();
     initSelectpickers();
     initFormOnLoad();
 
     $('#scanForPeriod').click(function () {
-                initDateEndField();
-        $('#departureDateEnd').click();
-            });
+        initDateEndField();
+        if ($('#scanForPeriod').prop("checked")) {
+            $('#departureDateEnd').datepicker("show");
+        }
+    });
 
     $('#searchButton').click(function () {
-            saveFormDataOnSearch();
-            $('#dateSlider').empty();
-            $('#resultTable').empty();
-            searchData();
-        });
+        saveFormDataOnSearch();
+        $('#dateSlider').empty();
+        $('#resultTable').empty();
+        $('#messageBox').empty();
+        $('#messageBox').hide();
+        searchData();
+    });
     $("#swapCities").click(function () {
         swapCities();
     });
@@ -24,7 +28,7 @@ function initDateEndField() {
     if ($('#scanForPeriod').prop("checked")) {
         $('#departureDateEnd').removeAttr('disabled');
         setDatePickerValue("departureDateEnd", strToDate(localStorage.getItem('requestForm.departureDateEnd')));
-        $('#departureDateEnd').datepicker("show");
+        // $('#departureDateEnd').datepicker("show");
     }
     else {
         $('#departureDateEnd').attr('disabled', true);
@@ -65,37 +69,36 @@ function initDatePickers() {
     });
 }
 
-function initSelectpickers(){
-$('.selectpicker').selectpicker({
-    size: 15,
-    liveSearch: true
-
-               });
-
-$('#fromCity').change(function () {
-            loadRoutes($('#toCity'), $('#fromCity').val(), 'dep');
+function initSelectpickers() {
+    $('.selectpicker').selectpicker({
+        size: 15,
+        liveSearch: true
     });
-$("#langSelect").change(function(){
-    changeLang($("#langSelect").val());
-});
-  /*  $('#toCity').change(function () {
 
-            loadRoutes($('#fromCity'), $('#toCity').val(), 'dst');
+    $('#fromCity').change(function () {
+        loadRoutes($('#toCity'), $('#fromCity').val(), 'dep');
+    });
+    $("#langSelect").change(function () {
+        changeLang($("#langSelect").val());
+    });
+    /*  $('#toCity').change(function () {
 
-    });*/
+     loadRoutes($('#fromCity'), $('#toCity').val(), 'dst');
+
+     });*/
 }
 
-function changeLang(newLang){
-var url = document.URL;
-var re = /(lang=)\w\w/gi;
-if(url.indexOf('lang=') != -1){
-       url = url.replace(re,'lang='+newLang);
-  }
-else{
-      url += '?lang='+newLang;
-  }
+function changeLang(newLang) {
+    var url = document.URL;
+    var re = /(lang=)\w\w/gi;
+    if (url.indexOf('lang=') != -1) {
+        url = url.replace(re, 'lang=' + newLang);
+    }
+    else {
+        url += '?lang=' + newLang;
+    }
 
-window.location = url;
+    window.location = url;
 }
 
 function swapCities() {
@@ -110,8 +113,8 @@ function initFormOnLoad() {
     if (localStorage.getItem('requestForm.fromCity') != null) {
         $('#fromCity').selectpicker('val', localStorage.getItem('requestForm.fromCity'));
 
-        $('#toCity').selectpicker('val',localStorage.getItem('requestForm.toCity'));
-       // loadRoutes($('#toCity'), $('#fromCity').val(), 'dep');
+        $('#toCity').selectpicker('val', localStorage.getItem('requestForm.toCity'));
+        // loadRoutes($('#toCity'), $('#fromCity').val(), 'dep');
 
         setDatePickerValue("departureDate", strToDate(localStorage.getItem('requestForm.departureDate')));
 
@@ -122,7 +125,7 @@ function initFormOnLoad() {
     initDateEndField();
 }
 
-function saveFormDataOnSearch(){
+function saveFormDataOnSearch() {
     if (localStorage) {
         localStorage.setItem('requestForm.fromCity', $('#fromCity').val());
         localStorage.setItem('requestForm.toCity', $('#toCity').val());
@@ -134,7 +137,7 @@ function saveFormDataOnSearch(){
     }
 }
 function strToDate(dateStr) {
-    if(dateStr == null || dateStr == ''){
+    if (dateStr == null || dateStr == '') {
         return null;
     }
     var parts = dateStr.split("-");
@@ -146,17 +149,18 @@ function dateToStr(date) {
     function pad(s) {
         return (s < 10) ? '0' + s : s;
     }
+
     var month = date.getMonth() + 1;
     return pad(date.getDate()) + "-" + pad(month) + "-" + date.getFullYear();
 }
 
 function setDatePickerValue(datePicker, date) {
     if (date != null) {
-            var today = new Date();
-            today.setHours(0,0,0,0)
-            if(date < today){
-                date = today;
-            }
+        var today = new Date();
+        today.setHours(0, 0, 0, 0)
+        if (date < today) {
+            date = today;
+        }
         $('#' + datePicker).datepicker('setDate', date);
     }
 }
@@ -201,19 +205,19 @@ function fillDateSlider() {
     });
 }
 /*calculates the first date for date slider depends on current date*/
-function getFirstDateForSlider(selectedDate, daysRange){
+function getFirstDateForSlider(selectedDate, daysRange) {
     var firstDate = new Date(selectedDate);
-    firstDate.setDate(selectedDate.getDate() - (Math.floor(daysRange/2)));
+    firstDate.setDate(selectedDate.getDate() - (Math.floor(daysRange / 2)));
 
     var today = new Date();
-    today.setHours(0,0,0,0)
-    while(firstDate < today){
-        firstDate.setDate(firstDate.getDate()+1);
+    today.setHours(0, 0, 0, 0)
+    while (firstDate < today) {
+        firstDate.setDate(firstDate.getDate() + 1);
     }
     return firstDate;
 }
 /*change color of slider block that represents selected in filter date*/
-function updateSliderCurrentDate(sliderContainer, selectedDate){
+function updateSliderCurrentDate(sliderContainer, selectedDate) {
     $(sliderContainer).find("div.date-slider-block[departuredate='" + selectedDate + "']").addClass("date-slider-block-selected-date");
 }
 
@@ -228,6 +232,11 @@ function updateSliderWeekDays(sliderContainer) {
     }
 }
 
+
+function displayMessage(message) {
+    $("#messageBox").text(message);
+    $("#messageBox").show();
+}
 function switchToResultList(date) {
     setDatePickerValue("departureDate", strToDate(date));
     $('#scanForPeriod').prop("checked", false);
@@ -256,50 +265,50 @@ function onDateSliderClick(div) {
 }
 /*
 
-function fillDateSliderAjax() {
-    var daysRange = 5;
+ function fillDateSliderAjax() {
+ var daysRange = 5;
 
-    var form = $('#requestForm');
-    var url = form.attr("action");
-    var formData = {};
-    formData["fromCity"] = $('#fromCity').val();
-    formData["toCity"] = $('#toCity').val();
-    formData["scanForPeriod"] = true;
+ var form = $('#requestForm');
+ var url = form.attr("action");
+ var formData = {};
+ formData["fromCity"] = $('#fromCity').val();
+ formData["toCity"] = $('#toCity').val();
+ formData["scanForPeriod"] = true;
 
-     var departureDate = strToDate($('#departureDate').val());
-     var departureDateEnd = new Date(departureDate.getTime());
-     departureDate.setDate(departureDate.getDate() - daysRange);
-     departureDateEnd.setDate(departureDateEnd.getDate() + daysRange);
+ var departureDate = strToDate($('#departureDate').val());
+ var departureDateEnd = new Date(departureDate.getTime());
+ departureDate.setDate(departureDate.getDate() - daysRange);
+ departureDateEnd.setDate(departureDateEnd.getDate() + daysRange);
 
-     formData["departureDate"] = dateToStr(departureDate);
-         formData["departureDateEnd"] = dateToStr(departureDateEnd);
+ formData["departureDate"] = dateToStr(departureDate);
+ formData["departureDateEnd"] = dateToStr(departureDateEnd);
 
-    jQuery.ajax({
-        url: url,
-        type: "POST",
-        data: JSON.stringify(formData),
-        contentType: "application/json",
-        dataType : 'json',
-        success: function (data) {
-            buildDateSlider(data);
-        }
-    });
-}
+ jQuery.ajax({
+ url: url,
+ type: "POST",
+ data: JSON.stringify(formData),
+ contentType: "application/json",
+ dataType : 'json',
+ success: function (data) {
+ buildDateSlider(data);
+ }
+ });
+ }
 
-function buildDateSlider(data){
+ function buildDateSlider(data){
 
-for (var el, i = 0; i < data.length; i++) {
-var dayBlock= document.createElement('div');
-dayBlock.className='col-lg-1';
-dayBlock.innerHTML = data[i].departureDate + " "+data[i].price;
-$("#dateslider").append(dayBlock);
+ for (var el, i = 0; i < data.length; i++) {
+ var dayBlock= document.createElement('div');
+ dayBlock.className='col-lg-1';
+ dayBlock.innerHTML = data[i].departureDate + " "+data[i].price;
+ $("#dateslider").append(dayBlock);
 
-}
+ }
 
 
 
-}
-*/
+ }
+ */
 
 
 function updateData() {
@@ -320,42 +329,41 @@ function sortResultList(sortBy, sortOrder) {
 
     var list = $("#resulList").find(".list-group-item");
 
-if(sortBy == "priceVal" && sortOrder == "ASC"){
-    list.sort(compareByPrice);
+    if (sortBy == "priceVal" && sortOrder == "ASC") {
+        list.sort(compareByPrice);
     }
-else if(sortBy == "priceVal" && sortOrder == "DESC"){
+    else if (sortBy == "priceVal" && sortOrder == "DESC") {
         list.sort(compareByPriceDESC);
-        }
-else if(sortBy == "timeVal"  && sortOrder == "ASC"){
-    list.sort(compareByTime);
     }
-else if(sortBy == "timeVal"  && sortOrder == "DESC"){
-    list.sort(compareByTimeDESC);
+    else if (sortBy == "timeVal" && sortOrder == "ASC") {
+        list.sort(compareByTime);
+    }
+    else if (sortBy == "timeVal" && sortOrder == "DESC") {
+        list.sort(compareByTimeDESC);
     }
 
     $("#resulList").html(list);
-    }
-
-
-
-function compareByPrice (a, b){
-    a = $(a).attr("priceVal");
-    b = $(b).attr("priceVal");
-
-return (a - b);
-
-}
-
-function compareByPriceDESC (a, b){
-    a = $(a).attr("priceVal");
-    b = $(b).attr("priceVal");
-
-return (b - a);
-
 }
 
 
-function compareByTime (a, b){
+function compareByPrice(a, b) {
+    a = $(a).attr("priceVal");
+    b = $(b).attr("priceVal");
+
+    return (a - b);
+
+}
+
+function compareByPriceDESC(a, b) {
+    a = $(a).attr("priceVal");
+    b = $(b).attr("priceVal");
+
+    return (b - a);
+
+}
+
+
+function compareByTime(a, b) {
     a = $(a).attr("timeVal");
     var splA = a.split(":");
     var dtA = new Date();
@@ -364,25 +372,25 @@ function compareByTime (a, b){
 
     b = $(b).attr("timeVal");
     var splB = b.split(":");
-        var dtB = new Date();
-        dtB.setHours(splB[0]);
-        dtB.setMinutes(splB[1]);
-     return (dtA - dtB);
+    var dtB = new Date();
+    dtB.setHours(splB[0]);
+    dtB.setMinutes(splB[1]);
+    return (dtA - dtB);
 }
 
-function compareByTimeDESC (a, b){
+function compareByTimeDESC(a, b) {
     a = $(a).attr("timeVal");
-       var splA = a.split(":");
-       var dtA = new Date();
-       dtA.setHours(splA[0]);
-       dtA.setMinutes(splA[1]);
+    var splA = a.split(":");
+    var dtA = new Date();
+    dtA.setHours(splA[0]);
+    dtA.setMinutes(splA[1]);
 
-       b = $(b).attr("timeVal");
-       var splB = b.split(":");
-           var dtB = new Date();
-           dtB.setHours(splB[0]);
-           dtB.setMinutes(splB[1]);
-        return (dtB - dtA);
+    b = $(b).attr("timeVal");
+    var splB = b.split(":");
+    var dtB = new Date();
+    dtB.setHours(splB[0]);
+    dtB.setMinutes(splB[1]);
+    return (dtB - dtA);
 }
 
 function buyTicket(link) {
@@ -394,14 +402,14 @@ function loadRoutes(container, value, depDst) {
 
     $.post(url, {'cityId': value, 'depDst': depDst})
         .done(function (data) {
-        $(container).empty();
+            $(container).empty();
             var i = 0;
 
-var opt = document.createElement("option");
-                opt.value = "";
-                opt.style ="hidden";
-                opt.innerHTML = " ";
-                $(container).append(opt);
+            var opt = document.createElement("option");
+            opt.value = "";
+            opt.style = "hidden";
+            opt.innerHTML = " ";
+            $(container).append(opt);
 
             for (; i < data.length; i++) {
 
@@ -410,15 +418,15 @@ var opt = document.createElement("option");
                 opt.innerHTML = data[i].cityName;
                 $(container).append(opt);
             }
-        $(container).selectpicker('refresh');
-        $(container).selectpicker('toggle');
+            $(container).selectpicker('refresh');
+            $(container).selectpicker('toggle');
 
         });
 
 }
 
 
-function setupEnv(){
+function setupEnv() {
     $.ajaxSetup({
         beforeSend: function () {
             //$("#messageBox").html("loading...");
@@ -429,13 +437,13 @@ function setupEnv(){
             //$("#messageBox").hide();
             $("#loadingIndicator").hide();
         },
-        error : function(jqXHR, textStatus, errorThrown) {
-         $("#loadingIndicator").hide();
-        if (jqXHR.status == 404) {
-            $("#messageBox").html("server not found");
-        } else {
-            $("#messageBox").html("Error: " + textStatus + ": " + errorThrown);
+        error: function (jqXHR, textStatus, errorThrown) {
+            $("#loadingIndicator").hide();
+            if (jqXHR.status == 404) {
+                $("#messageBox").html("server not found");
+            } else {
+                $("#messageBox").html("Error: " + textStatus + ": " + errorThrown);
+            }
         }
-    }
     });
 }
